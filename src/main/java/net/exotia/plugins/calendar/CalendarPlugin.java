a55d37a6c968fc2246f0588eab5a04f07e783d29
Wiktor <mcserw4@wp.pl>
2023-08-11 11:34:09 +0200

    cleared repo

--- src/main/java/net/exotia/plugins/calendar/CalendarPlugin.java
+++ src/main/java/net/exotia/plugins/calendar/CalendarPlugin.java
@@ -1,97 +1,10 @@
 package net.exotia.plugins.calendar;
 
-import dev.rollczi.litecommands.bukkit.LiteBukkitFactory;
-import dev.rollczi.litecommands.bukkit.tools.BukkitOnlyPlayerContextual;
-import dev.rollczi.litecommands.bukkit.tools.BukkitPlayerArgument;
-import eu.okaeri.injector.Injector;
-import eu.okaeri.injector.OkaeriInjector;
-import lombok.Getter;
-import net.exotia.plugins.calendar.calendar.gui.GuiCalendar;
-import net.exotia.plugins.calendar.command.CommandCalendar;
-import net.exotia.plugins.calendar.command.CommandReload;
-import net.exotia.plugins.calendar.configuration.ConfigurationFactory;
-import net.exotia.plugins.calendar.configuration.ConfigurationGui;
-import net.exotia.plugins.calendar.configuration.ConfigurationMessage;
-import net.exotia.plugins.calendar.configuration.ConfigurationRewards;
-import net.exotia.plugins.calendar.handler.HandlerInvalid;
-import net.exotia.plugins.calendar.handler.HandlerUnauthorized;
-import net.exotia.plugins.calendar.listener.ListenerJoin;
-import net.exotia.plugins.calendar.utils.UtilMessage;
-import net.kyori.adventure.platform.bukkit.BukkitAudiences;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.stream.Stream;
-
-public final class CalendarPlugin extends JavaPlugin {
-    private final Injector injector = OkaeriInjector.create();
-    @Getter
-    private static Plugin plugin;
-    @Getter
-    private static BukkitAudiences audiences;
-    private final ConfigurationFactory configurationFactory = new ConfigurationFactory(this.getDataFolder());
-    private ConfigurationMessage configurationMessage;
-    private ConfigurationGui configurationGui;
-    private ConfigurationRewards configurationRewards;
-
-    @Override
-    public void onEnable() {
-        plugin = this;
-        audiences = BukkitAudiences.create(this);
-
-        injector.registerInjectable(plugin);
-        injector.registerInjectable(injector);
-        injector.registerInjectable(configurationFactory);
-
-        setupConfiguration();
-        setupUtils();
-        setupCommands();
-        setupEvents();
-    }
-
+public class CalendarPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
-        cleanUp();
-    }
-
-    private void setupConfiguration() {
-        configurationMessage = configurationFactory.produce(ConfigurationMessage.class, "messages.yml");
-        configurationGui = configurationFactory.produce(ConfigurationGui.class, "guis.yml");
-        configurationRewards = configurationFactory.produce(ConfigurationRewards.class, "rewards.yml");
-
-        injector.registerInjectable(configurationMessage);
-        injector.registerInjectable(configurationGui);
-        injector.registerInjectable(configurationRewards);
-    }
-
-    private void setupUtils() {
-        injector.registerInjectable(injector.createInstance(GuiCalendar.class));
-    }
-
-    private void setupCommands() {
-        LiteBukkitFactory.builder(this.getServer(), "exotia.net")
-            .argument(Player.class, new BukkitPlayerArgument<>(this.getServer(), UtilMessage.getMessage(configurationMessage.getCommandsPlayer().getOffline())))
-            .contextualBind(Player.class, new BukkitOnlyPlayerContextual<>(UtilMessage.getMessage(configurationMessage.getCommandsPlayer().getOnly())))
-            .commandInstance(
-                    injector.createInstance(CommandReload.class),
-                    injector.createInstance(CommandCalendar.class)
-            )
-            .invalidUsageHandler(injector.createInstance(HandlerInvalid.class))
-            .permissionHandler(injector.createInstance(HandlerUnauthorized.class))
-            .register();
-    }
-
-    private void setupEvents() {
-        Stream.of(
-            injector.createInstance(ListenerJoin.class)
-        ).forEach(listener -> Bukkit.getPluginManager().registerEvents(listener, this));
-    }
-
-    private void cleanUp() {
-        configurationMessage.save();
-        configurationGui.save();
-        configurationRewards.save();
+        super.onDisable();
     }
 }
